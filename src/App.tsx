245ff7e1b82def6cccfeb93a12dbfd9122802e25
mananjot-17 <mananjot.singh@aimlarger.com>
2025-02-03 16:31:24 +0530

    adding authersization

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import "./App.css";
 import Dashboard from "./Pages/Dashboard";
+import { SignInButton, SignedIn, SignedOut } from "@clerk/react-router";
 
 function App() {
   return (
     <div>
-      <Dashboard />
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <Dashboard />
+      </SignedIn>
     </div>
   );
 }
--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -3,6 +3,7 @@ import { useSpring, animated } from "react-spring";
 import Counter from "../components/Counter";
 import RichTextEditor from "../components/TextEditor";
 import UserForm from "../components/UserForm";
+import { UserButton } from "@clerk/clerk-react";
 
 const Dashboard = () => {
   const [userData, setUserData] = useState({
@@ -28,20 +29,27 @@ const Dashboard = () => {
   }, []);
 
   return (
-    <animated.div
-      style={fadeIn}
-      className="grid grid-cols-1 md:grid-cols-2 gap-6 p-6"
-    >
-      <animated.div className="p-6 bg-white rounded-xl shadow-lg transition-transform transform hover:scale-105 flex items-center justify-center">
-        <Counter />
+    <div className="p-6 bg-gray-200">
+      <div className="flex items-center justify-between mb-8 p-3 rounded-lg">
+        <h2 className="text-3xl font-medium ">Dashboard</h2>
+        <UserButton />
+      </div>
+
+      <animated.div
+        style={fadeIn}
+        className="grid grid-cols-1 md:grid-cols-2 gap-6"
+      >
+        <animated.div className="p-6 bg-white rounded-xl shadow-lg transition-transform transform hover:scale-105 flex items-center justify-center">
+          <Counter />
+        </animated.div>
+        <animated.div className="p-6 bg-white rounded-xl shadow-lg transition-transform transform hover:scale-105 flex items-center justify-center">
+          <RichTextEditor userData={userData} />
+        </animated.div>
+        <animated.div className="p-6 bg-white rounded-xl shadow-lg transition-transform transform hover:scale-105 flex items-center justify-center">
+          <UserForm userData={userData} setUserData={setUserData} />
+        </animated.div>
       </animated.div>
-      <animated.div className="p-6 bg-white rounded-xl shadow-lg transition-transform transform hover:scale-105 flex items-center justify-center">
-        <RichTextEditor userData={userData} />
-      </animated.div>
-      <animated.div className="p-6 bg-white rounded-xl shadow-lg transition-transform transform hover:scale-105 flex items-center justify-center">
-        <UserForm userData={userData} setUserData={setUserData} />
-      </animated.div>
-    </animated.div>
+    </div>
   );
 };
 
